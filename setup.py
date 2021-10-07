105ea66c66676eb729bd0456a84015778c8c4bd0
Jamie Titus <ctitus@stanford.edu>
2021-10-07 11:57:04 -0400

    setup.py corrected

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     requirements = f.read().split()
-    
+
 setup(
     author="NIST Staff",
     author_email=None,
-    description="Beamline hardware classes",
+    description="Beamline utility functions",
     install_requires=requirements,
-    name="bl_base",
+    name="bl_funcs",
     use_scm_version=True,
     packages=find_packages()
 )
