e1c6bc710a2f8f30114cb36d94c17b50664b90a8
Charles Titus <ctitus@bnl.gov>
2022-05-12 15:03:47 -0400

    adding SCM versioning back, and making it actually work

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author_email=None,
     description="Beamline utility functions",
     install_requires=requirements,
+    use_scm_version=True
     name="sst_funcs",
     packages=find_packages()
 )
