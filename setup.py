72b59ffb714ff48b58bb54ab3c17dab75ba56b3a
Charles Titus <ctitus@bnl.gov>
2022-05-13 16:18:21 -0400

    version info added, only works when package is installed

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email=None,
     description="Beamline utility functions",
     install_requires=requirements,
-    use_scm_version=True
+    use_scm_version=True,
     name="sst_funcs",
     packages=find_packages()
 )
