d6951a10a287a13f7fde02336cf15f903b0c4473
Eliot Gann <37706296+EliotGann@users.noreply.github.com>
2022-07-11 10:29:02 -0400

    mirror and grating calibration code added

--- sst_funcs/gGrEqns.py
+++ sst_funcs/gGrEqns.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy.optimize as opt
-from ..CommonFunctions.functions import run_report
+from .printing import run_report
 
 run_report(__file__)
 
@@ -14,333 +14,150 @@ assumes length units are in mm
 """
 
 
-def getAlphaDeg(eV, beta_deg, k_invmm, m):
-    # calculate alpha in deg
-    # eV: energy
-    # beta_deg: diffraction angle
-    # k_invmm: central line density
-    # m: diffraction order
-    hc = 0.0012398  # in units of eV mm
-    lambda_mm = hc / eV
-    beta = np.radians(beta_deg)
-    alpha = np.arcsin((m * k_invmm * lambda_mm - np.sin(beta)))
-    return np.degrees(alpha)
-
-
-def getBetaDeg(eV, alpha_deg, k_invmm, m):
+def getBetaDeg(en_eV, alpha_deg, k_invmm, m):
     # calculate beta in deg
-    # eV: energy
-    # alpha_deg: incident angle
-    # k_invmm: central line density
-    # m: diffraction order
+    # en_eV: energy units of electron Volts
+    # alpha_deg: incident angle in degrees
+    # k_invmm: central line density in mm^-1
+    # m: diffraction order unitless integer
     hc = 0.0012398  # in units of eV mm
-    lambda_mm = hc / eV
-    alpha = np.radians(alpha_deg)
-    beta = np.arcsin((m * k_invmm * lambda_mm - np.sin(alpha)))
+    lambda_mm = hc / en_eV  # wavelength in mm
+    alpha = np.radians(alpha_deg)  # alpha angle in radians
+    beta = np.arcsin((m * k_invmm * lambda_mm - np.sin(alpha)))  # beta angle in radians
     return np.degrees(beta)
 
 
-def getEv(alpha_deg, beta_deg, k_invmm, m):
-    # calculate energy in eV
-    # alpha_deg: incident angle
-    # beta_deg: diffraction angle
-    # k_invmm: central line density
-    # m: diffraction order
-    hc = 0.0012398  # in units of eV mm
-    alpha = np.radians(alpha_deg)
-    beta = np.radians(beta_deg)
-    lambda_mm = (1 / (m * k_invmm)) * (np.sin(alpha) + np.sin(beta))
-    eV = hc / lambda_mm
-    return eV
-
-
-def getMag(alpha_deg, beta_deg, ra, rb):
-    # calculate the magnification of the image
-    # alpha_deg: incident angle in deg
-    # beta_deg: diffracted angle in deg
-    # ra: entrance arm length (ra and rb must have same units)
-    # rb: exit arm length (ra and rb must have same units)
-    alpha = np.radians(alpha_deg)
-    beta = np.radians(beta_deg)
-    return (rb * np.cos(alpha)) / (ra * np.cos(beta))
-
-
-def getAngDisp(alpha_deg, beta_deg, k_invmm, m):
-    # calculate the energy dispersion in terms of eV/rad
-    # alpha_deg: incident angle in deg
-    # beta_deg: diffracted angle in deg
-    # k_invmm: central line density in mm-1
-    # m: diffraction order
-    hc = 0.0012398  # in units of eV mm
-    alpha = np.radians(alpha_deg)
-    beta = np.radians(beta_deg)
-    # angular dispersion dE/d(beta)
-    angDisp = -hc * k_invmm * m * np.cos(beta) * (np.sin(alpha) + np.sin(beta)) ** -2
-    return angDisp
-
-
-def getLinDisp(alpha_deg, beta_deg, k_invmm, m, rb_mm):
-    # calculate the linear energy dispersion at the exit slit
-    #   in units of eV/mm
-    # alpha_deg: incident angle in deg
-    # beta_deg: diffracted angle in deg
-    # k_invmm: central line density in mm-1
-    # m: diffraction order
-    # rb_mm: exit arm length in mm
-
-    angDisp = getAngDisp(alpha_deg, beta_deg, k_invmm, m)
-    linDisp = (1 / rb_mm) * angDisp
-    return linDisp
-
-
-def blazeAngDeg(eV, alpha_deg, k_invmm, m):
-    # calculate the required blaze angle in deg
-    # parameter list is self descriptive
-    # blaze angle is negative in negative order!
-    beta_deg = getBetaDeg(eV, alpha_deg, k_invmm, m)
-    return 0.5 * (alpha_deg + beta_deg)
-
-
-def blazeAngDeg2(eV, cff, k_invmm, m):
-    # calculate the required blaze angle in deg
-    # parameter list is self descriptive
-    # blaze angle is negative in negative order!
-    alpha_deg = ruben2005eqn8m(eV, cff, k_invmm, m)
-    beta_deg = getBetaDeg(eV, alpha_deg, k_invmm, m)
-    return 0.5 * (alpha_deg + beta_deg)
-
-
-def alphaOnBlazeA(eV, k_invmm, m, blaze_deg):
-    # calc on blaze alpha in deg for a given blaze angle analytically
-    # parameter list is self descriptive
-    # blaze angle is negative in negative order!
-    hc = 0.0012398  # in units of eV mm
-    lambda_mm = hc / eV
-    alpha = np.radians(blaze_deg) + np.arccos(
-        (m * k_invmm * lambda_mm) / (2 * np.sin(np.radians(blaze_deg)))
-    )
-    return np.degrees(alpha)
-
-
-def alphaOnBlaze(eV, k_invmm, m, blaze_deg):
-    # calculate the on blaze alpha in deg for a given blaze angle
-    # parameter list is self descriptive
-    # blaze angle is negative in negative order!
-    args = (eV, k_invmm, m, blaze_deg)
-    llim, ulim = onBlazeBracket(*args)
-    try:
-        result = opt.bisect(onBlazeFn, llim, ulim, args)
-    except:
-        result = float("nan")
-    return result
-
-
-def onBlazeFn(alpha_deg, *args):
-    # trancendental function to solve for alpha given blaze angle
-    # arg parameters are self descriptive
-    # *args = (eV, k_invmm, m, blaze_deg)
-    # blaze angle is negative for negative orders
-    eV = args[0]
-    k_invmm = args[1]
-    m = args[2]
-    blaze_deg = args[3]
-    #
-    bl = np.radians(blaze_deg)
-    alpha = np.radians(alpha_deg)
-    hc = 0.0012398  # in units of eV mm
-    lambda_mm = hc / eV
-    try:
-        result = np.degrees(
-            bl - 0.5 * (alpha + np.arcsin(m * k_invmm * lambda_mm - np.sin(alpha)))
-        )
-    except:
-        result = float("nan")
-    return result
-
-
-def onBlazeBracket(eV, k_invmm, m, blaze_deg):
-    # find the range in deg of valid values for the onBlaze numerical solver
-    # this is required for bracketing the numerical problem solver
-    # input parameters are self explanatory
-    alphaLow = 0.0
-    alphaHigh = 90.0
-    tolerance = 0.0000001
-    arg = (eV, k_invmm, m, blaze_deg)
-
-    # initial case test 0 and 90 degrees
-    testLow = onBlazeFn(alphaLow, *arg)
-    if np.isnan(testLow):
-        raise RuntimeError("No valid solution")
-    testHigh = onBlazeFn(alphaHigh, *arg)
-    if not np.isnan(testHigh):
-        return (0.0, 90.0)
-
-    # current state
-    #    alphaLow = 0
-    #    alphaHigh = nan
-    alphaLowOld = 0
-    while True:
-        alphaMid = (alphaHigh + alphaLow) / 2
-        # print (alphaLow, alphaMid, alphaHigh)
-        testMid = onBlazeFn(alphaMid, *arg)
-        if np.isnan(testMid):
-            # alphaLow does not change
-            alphaHigh = alphaMid
-        else:
-            # alphaHigh does not change
-            alphaLow = alphaMid
-            if abs(alphaLow - alphaLowOld) < tolerance:
-                break
-            else:
-                alphaLowOld = alphaLow
-    return (0, alphaLow)
-
-
-def ruben2005eqn8m(eV, c, k, m):
-    #   eqn 8, Ruben 2005, to calculate alpha from c
+def ruben2005eqn8m(en_eV, cff, k_invmm, m):
+    #   eqn 8, doi:10.1016/j.nima.2004.09.007, to calculate alpha from cff
     #   generalized to include higher orders
-    #   c is unitless
-    #   k is central line density in mm-1
-    #   m is the diffraction order, and integer
+    #   en_eV is the energy in electron volts
+    #   cff is unitless constant of fixed focus
+    #   k_invmm is central line density in mm-1
+    #   m is the diffraction order, integer, unitless
     #   returns alpha in degrees
     #   works for grazing incidence, 2000 eV, 1800 mm-1, cff=2, m=+1 thru +5
     #   works for grazing incidence, 2000 eV, 1800 mm-1, cff=0.2, m=-1 thru -5
-    aa = 1 / (c ** 2 - 1)
-    llambda = (12398 / eV) * 1e-7  # wavelength in mm
-    return np.degrees(
-        np.arcsin(-m * k * llambda * aa + np.sqrt(1 + (c * m * k * llambda * aa) ** 2))
-    )
-
-
-# needs error checking
-def ruben2005eqn9(eV, k, ra, rb, a1):
-    #   eqn 9, Ruben 2005, to calculate c to zero defocus term for given
-    #   geometry and plane VLS parameters for m = +1 (inside order)
-    #   k is central line density in mm-1
-    #   ra and rb are in mm
-    #   a1 is VLS focusing term in mm-2
-    rr = rb / ra
-    A0 = k * (12398 / eV) * 1e-7
-    A2 = -(1 / 2) * (12398 / eV) * 1e-7 * rb * a1
-    t1 = 2 * A2
-    t2 = 4 * (A2 / A0) ** 2
-    t3 = (4 + 2 * A2 - A0 ** 2) * rr
-    t4 = -4 * (A2 / A0) * np.sqrt((1 + rr) ** 2 + 2 * A2 * (1 + rr) - A0 ** 2 * rr)
-    t5 = -4 + A0 ** 2 - 4 * A2 + 4 * (A2 / A0) ** 2
-    return np.sqrt((t1 + t2 + t3 + t4) / t5)
-
-
-##############################################################################
-
-
-# for beamline soft xray grating instruments
-
-
-def softXrayMono1(eV, k, m, c, rb_mm, bounce, inOff_deg, outOff_deg, verbose):
-    """
-    # calculate premirror and grating angles for NSLS-II soft xray monos
-    # eV: energy
-    # k: central line density in mm-1
-    # m: diffraction order
-    # c: cff 0 < cff < infinity
-    # bounce = 'up' or 'down'
-    # inOff_deg - input beam angle relative to horizontal, NSLSII sense
-    # outOff_deg - output beam angle relative to horizontal, NSLSII sense
-    """
-    # correct for several energies for Centurion
-    # correctly reverses sign of angles if geometry is flipped upside-down
-
-    # consider bounce direction
-    if bounce == "up":
-        a = -1
-    elif bounce == "down":
-        a = +1
-    else:
-        a = float("nan")
-
-    # calculate angles, no offsets
-    alpha_deg = ruben2005eqn8m(eV, c, k, m)
-    beta_deg = getBetaDeg(eV, alpha_deg, k, m)
-    # include offsets
-    thetaPMinc_deg = abs(
-        +0.5 * (outOff_deg - inOff_deg + a * (180.0 - alpha_deg + beta_deg))
-    )
-    thetaPM_deg = +0.5 * (outOff_deg + inOff_deg + a * (180.0 - alpha_deg + beta_deg))
-    thetaGR_deg = a * (90.0 + beta_deg) + outOff_deg
-    disp = getLinDisp(alpha_deg, beta_deg, k, m, rb_mm)
-    if verbose:
-        # alpha, beta both relative to normal and surface
-        print("eV=", eV, "c=", c)
-        print("alpha=", alpha_deg, 90.0 - alpha_deg)
-        print("beta=", beta_deg, (90 + beta_deg))
-        print("incident angle on pm=", thetaPMinc_deg)
-        print("dispersion (eV/mm) =", disp)
-        # grating and premirror rotation angles
-        print("rotation angles relative to horizontal")
-        print("     premirror", thetaPM_deg)
-        print("     grating", thetaGR_deg)
-    return (thetaPM_deg, thetaGR_deg, alpha_deg, beta_deg, thetaPMinc_deg, disp)
-
-
-def softXrayMono2(k_invmm, m, grAng_deg, pmAng_deg, bounce, inOff_deg, outOff_deg):
-    """
-    # calculate the energy for a given premirr and grating angle for a PGM
-    # the angles are relative to the horizontal, using NSLS-II standard
-    # k_invmm: central line density in mm-1
-    # m: diffraction order
-    # grAng_deg: grating angle, relative to horizontal
-    # pmAng_deg: premirror angle, relative to horizontal
-    # bounce = 'up' or 'down'
-    # inOff_deg - input beam angle relative to horizontal, NSLSII sense
-    # outOff_deg - output beam angle relative to horizontal, NSLSII sense
-    """
-    ### correct for several energies for Centurion
-    ### correctly reverses sign of angles if geometry is flipped upside-down
-    # not error checked yet
-
-    # consider bounce direction
-    if bounce == "up":
-        a = -1
-    elif bounce == "down":
-        a = +1
-    else:
-        a = float("nan")
-
-    beta_deg = -90 + a * (grAng_deg - outOff_deg)
-    alpha_deg = +180 + beta_deg + a * (outOff_deg + inOff_deg - 2 * pmAng_deg)
-    beta = np.radians(beta_deg)
-    alpha = np.radians(alpha_deg)
-    lambda_mm = (np.sin(alpha) + np.sin(beta)) / (m * k_invmm)
+    aa = 1 / (cff ** 2 - 1)  ## unitless
     hc = 0.0012398  # in units of eV mm
-    eV = hc / lambda_mm
-    if alpha_deg > 90:
-        eV = np.nan
-    return eV
+    lambda_mm = hc / en_eV  ## in mm
+    return np.degrees(np.arcsin(-m * k_invmm * lambda_mm * aa + np.sqrt(1 + (cff * m * k_invmm * lambda_mm * aa) ** 2)))
 
 
-# conditional code
-def getAlphaDegH(eV, k_invmm, m):
-    # calculate the horizon angle for alpha for grazing incidence
-    # for outside diffraction orders, as alpha goes more to grazing, eventually
-    # beta will become 90 degrees - I call this the horizon angle for alpha
-    # eV: energy
-    # k_invmm: central line density
-    # m: diffraction order
-    hc = 0.0012398  # in units of eV mm
-    lambda_mm = hc / eV
-    alpha = np.arcsin((m * k_invmm * lambda_mm + 1))
-    return np.degrees(alpha)
-
-
-# Eliot adding this for getting the mirror and grating angles for the monochromator based on energy, c, m, and k
-def get_mirror_grating_angles(eV, c, m, k):
-    grating_angle = (
-        -(
-            180
-            - ruben2005eqn8m(eV, c, k, m)
-            + getBetaDeg(eV, ruben2005eqn8m(eV, c, k, 1), k, m)
-        )
-        / 2
-    )
-    mirror_angle = -90 - getBetaDeg(eV, ruben2005eqn8m(eV, c, k, m), k, m)
+def get_mirror_grating_angles(en_eV, cff, k_invmm, m):
+    a = ruben2005eqn8m(en_eV, cff, k_invmm, m)  # twice the angle between mirror and grating in equation land
+    b = getBetaDeg(en_eV, a, k_invmm, m)  # angle of pre-mirror (in degrees) in equation land
+    mirror_angle = -(180 - a + b) / 2  # tangle of the mirror pitch for the actual motor (degrees)
+    grating_angle = -90 - b  # angle of the grating pitch for the actual motor (degrees)
     return [mirror_angle, grating_angle]
+
+
+def energy(mirror,grating, k_invmm, m):
+    hc = 0.0012398  # in units of eV mm
+    b = np.radians(-90 - grating)
+    a = np.radians(2 * mirror + 90 - grating)
+    return hc * k_invmm * m / (np.sin(a) + np.sin(b))
+
+
+def find_best_offsets(mirror_pitches, grating_pitches, mguesses, eVs, k_invmm):
+    '''
+    given a bunch of measurements of mirror and grating pitches, fits the most likely constant offsets
+    for the mirror and grating pitch.  Each measurement can be at a different order(m) and a different energy
+    the grating parameters should not change, so only the constant line density is needed
+
+    mirror_pitches : list or array of length n
+        experimental mirror pitch angles
+
+    grating_pitches : list or array of length n
+        experimental grating pitch angles
+
+    m_guesses : list or array of length n
+        the diffraction order which was scanned
+
+    eVs : list or array of length n
+        the energy of each measurement (allows for different calibrants)
+
+    k_invmm : single number the central spacing of the grating in mm^-1
+
+    output: the result of numpy.optimize = has a lot of components which might be useful, the most obvious of which are
+        output.x the optimized offsets of the mirror, and grating pitch
+        output.success wether the optimize function thinks that it succeeded or not.  experince shows that good fits are still possible when this is false
+    '''
+    fit_result = opt.minimize(diffraction_pitch_offset_error_func,
+                              x0=[0.0001, -0.0001],
+                              args=(
+                                  mirror_pitches,
+                                  grating_pitches,
+                                  mguesses,
+                                  eVs,
+                                  k_invmm
+                              )
+                              )
+    return fit_result
+
+
+
+def diffraction_pitch_offset_error_func(
+        fit_elements,
+        mirror_pitches,
+        grating_pitches,
+        m_guesses,
+        en_eVs,
+        k_invmm):
+    '''
+    fit_elements, an 2 element array:
+        mirror angle offset,
+        grating angle offset
+
+    mirror_pitches : list or array of length n
+        experimental mirror pitch angles
+
+    grating_pitches : list or array of length n
+        experimental grating pitch angles
+
+    m_guesses : list or array of length n
+        the diffraction order which was scanned
+
+    en_eV : list or array of length n
+        the energy of each measurement (allows for different calibrants)
+
+    k_invmm : single number the central spacing of the grating in mm^-1
+    '''
+    error = 0
+    for mp, gp, m, en_eV in zip(mirror_pitches, grating_pitches, m_guesses, en_eVs):
+        mir = mp - fit_elements[0]  # the test mirror position
+        grat = gp - fit_elements[1]  # the test grating position
+        en_theoretical = energy(mir,grat, k_invmm, m)  # what energy these test positions would produce
+        error += (en_theoretical - en_eV) ** 2
+    return error
+
+
+def test_grating_fit(numpoints, minc, maxc, minm, maxm, energy, k, noise, moffset, goffset):
+    # create some fake measurements of grating and mirror positions to test the fit function
+
+    cs = np.linspace(minc, maxc, numpoints)  # used to generate test pairs of pitches
+    ms = np.round(np.linspace(minm, maxm, numpoints))
+    evs = np.ones(numpoints) * energy
+    merr = np.random.normal(0, noise, numpoints)
+    gerr = np.random.normal(0, noise, numpoints)
+
+    mirrors, gratings = list(zip(*[get_mirror_grating_angles(energy, c, k, m) for c, m in zip(cs, ms)]))
+    mirrors += merr + moffset
+    gratings += gerr + goffset
+    return find_best_offsets(mirrors, gratings, ms, evs, k)
+
+
+
+
+'''
+mirror positions: [-3.6558274763042604, -3.3921585294849734, -3.1876054822737245, -3.0234213066329048, -2.8882065368134207, -2.774584471608641, -2.6775464182938222, -2.5935586706589646]
+grating positions: [-4.107032615400001, -3.880574386500001, -3.709388039800004, -3.5753463783000043, -3.4671052284000012, -3.379157631200002, -3.3047008284000015, -3.242455403500003]
+energy positions: [291.65, 291.65, 291.65, 291.65, 291.65, 291.65, 291.65, 291.65]
+orders: [1, 1, 1, 1, 1, 1, 1, 1]
+
+
+mirror positions: [-3.234388850111962, -3.0810948898872326, -2.9527235798924423, -4.572999932948626, -4.3563799893796045, -4.1749665476012225]                                             
+grating positions: [-3.7760457088000052, -3.6498169308000072, -3.546186037900007, -5.3394722246000015, -5.160893796100005, -5.014527626700001]
+energy positions: [291.65, 291.65, 291.65, 291.65, 291.65, 291.65]
+orders: [1, 1, 1, 2, 2, 2]
+
+'''
