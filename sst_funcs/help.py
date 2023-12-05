0a7cd4320f2ae3d69e26eca204768b4b93687ffb
Charles Titus <ctitus@bnl.gov>
2023-12-05 18:08:29 -0500

    help functions updated

--- sst_funcs/help.py
+++ sst_funcs/help.py
@@ -1,6 +1,7 @@
 from sst_funcs.printing import boxed_text
 
-GLOBAL_HELP_DICTIONARY = {'functions': {}, 'plans': {}, 'scans': {}}
+GLOBAL_HELP_DICTIONARY = {'functions': {}, 'plans': {}, 'scans': {}, 'xas': {}}
+GLOBAL_IMPORT_DICTIONARY = {}
 
 
 def add_to_func_list(f):
@@ -8,8 +9,9 @@ def add_to_func_list(f):
     A function decorator that will add the function to the built-in list
     """
     key = f.__name__
-    doc = f.__doc__
+    doc = f.__doc__ if f.__doc__ is not None else "No Docstring yet!"
     GLOBAL_HELP_DICTIONARY['functions'][key] = doc
+    GLOBAL_IMPORT_DICTIONARY[key] = f
     return f
 
 
@@ -18,8 +20,9 @@ def add_to_plan_list(f):
     A function decorator that will add the plan to the built-in list
     """
     key = f.__name__
-    doc = f.__doc__
+    doc = f.__doc__ if f.__doc__ is not None else "No Docstring yet!"
     GLOBAL_HELP_DICTIONARY['plans'][key] = doc
+    GLOBAL_IMPORT_DICTIONARY[key] = f
     return f
 
 
@@ -28,8 +31,19 @@ def add_to_scan_list(f):
     A function decorator that will add the plan to the built-in list
     """
     key = f.__name__
-    doc = f.__doc__
+    doc = f.__doc__ if f.__doc__ is not None else "No Docstring yet!"
     GLOBAL_HELP_DICTIONARY['scans'][key] = doc
+    GLOBAL_IMPORT_DICTIONARY[key] = f
+    return f
+
+def add_to_xas_list(f):
+    """
+    A function decorator that will add the plan to the built-in list
+    """
+    key = f.__name__
+    doc = f.__doc__ if f.__doc__ is not None else "No Docstring yet!"
+    GLOBAL_HELP_DICTIONARY['xas'][key] = doc
+    GLOBAL_IMPORT_DICTIONARY[key] = f
     return f
 
 
@@ -42,5 +56,23 @@ def print_builtins(sections=None):
     if type(sections) is str:
         sections = [sections]
     for key in sections:
+        textList = []
         section = f"{key.capitalize()}"
-        boxed_text(section, sorted(GLOBAL_HELP_DICTIONARY[key].keys()), "white")
+        if key == 'xas':
+            for f in sorted(GLOBAL_HELP_DICTIONARY[key].keys()):
+                doc = GLOBAL_IMPORT_DICTIONARY[f]._short_doc
+                textList.append(f"{f}: {doc}")
+
+        else:
+            for f in sorted(GLOBAL_HELP_DICTIONARY[key].keys()):
+                doc = GLOBAL_HELP_DICTIONARY[key][f].split('\n')[0]
+                textList.append(f"{f}: {doc}")
+        boxed_text(section, textList, "white")
+
+@add_to_func_list
+def beamline_help():
+    print('Welcome to SST. For a list of loaded functions and plans, call print_builtins() \n' \
+          'To print the docstring for any of the built-in functions, use the built-in python "?"'\
+          ' command with the name of the desired function. \n I.e, typing activate_detector? will '\
+          'print the help text for the "activate_detector" function')
+        
