12bb8ab862d209843bcbcd4796e18788defc60ea
Charles Titus <ctitus@bnl.gov>
2023-12-19 17:00:46 -0500

    Added ability to pass a filename into object configuration system, so that multiple config files can be used

--- sst_funcs/configuration.py
+++ sst_funcs/configuration.py
@@ -8,16 +8,21 @@ GLOBAL_CONF_DB = {}
 
 # configfile = join(dirname(__file__), "config.yaml")
 # configdb = loadConfigDB(configfile)
-def loadConfigDB(filename):
+def loadConfigDB(filename, saveGlobal=True):
     global GLOBAL_CONF_DB
     with open(filename) as f:
         db = yaml.safe_load(f)
-    GLOBAL_CONF_DB = db
+    if saveGlobal:
+        GLOBAL_CONF_DB = db
     return db
 
 
-def getConfigDB():
-    return GLOBAL_CONF_DB
+def getConfigDB(filename=None):
+    if filename is not None:
+        db = loadConfigDB(filename, saveGlobal=False)
+    else:
+        db = GLOBAL_CONF_DB
+    return db
 
 
 def simpleResolver(fullclassname):
@@ -28,8 +33,8 @@ def simpleResolver(fullclassname):
     return cls
 
 
-def getObjConfig(name):
-    confdb = getConfigDB()
+def getObjConfig(name, filename=None):
+    confdb = getConfigDB(filename)
     for objdb in confdb.values():
         if name in objdb:
             objconf = deepcopy(objdb.get("_default", {}))
@@ -38,8 +43,8 @@ def getObjConfig(name):
     return None
 
 
-def getGroupConfig(group_name):
-    objdb = getConfigDB()[group_name]
+def getGroupConfig(group_name, filename=None):
+    objdb = getConfigDB(filename)[group_name]
     devicedb = {}
     for name in objdb:
         if name == '_default':
@@ -50,8 +55,8 @@ def getGroupConfig(group_name):
     return devicedb
 
 
-def findAndLoadDevice(name, cls=None):
-    device_info = getObjConfig(name)
+def findAndLoadDevice(name, cls=None, filename=None):
+    device_info = getObjConfig(name, filename=filename)
     return instantiateDevice(device_info, cls)
 
 
@@ -60,13 +65,18 @@ def instantiateDevice(device_info, cls=None):
         device_info.pop("_target_")
         prefix = device_info.pop("prefix", "")
         return cls(prefix, **device_info)
-    elif device_info['_target_'] is not None:
+    elif device_info.get('_target_', None) is not None:
         cls = simpleResolver(device_info.pop('_target_'))
         prefix = device_info.pop('prefix', '')
         return cls(prefix, **device_info)
+    else:
+        raise KeyError("Could not find '_target_' in {}".format(device_info))
 
 
-def instantiateGroup(group_name, namedict, cls=None):
-    group_config = getGroupConfig(group_name)
+def instantiateGroup(group_name, namedict=None, cls=None, filename=None):
+    group_config = getGroupConfig(group_name, filename=filename)
+    if namedict is None:
+        namedict = {}
     for device_name, device_info in group_config.items():
         namedict[device_name] = instantiateDevice(device_info)
+    return namedict
