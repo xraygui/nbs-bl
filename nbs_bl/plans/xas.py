5e77ddad8f05bdb552052cd416f20b9fe4794fb5
Charles Titus <ctitus@bnl.gov>
2024-11-03 11:45:11 -0500

    Fixed load_xas so that it dumps plans into the global ipython namespace

--- nbs_bl/plans/xas.py
+++ nbs_bl/plans/xas.py
@@ -16,6 +16,8 @@ try:
 except ModuleNotFoundError:
     import tomli as tomllib
 
+import inspect
+
 GLOBAL_XAS_PLANS = GLOBAL_USER_STATUS.request_status_dict("XAS_PLANS", use_redis=True)
 
 
@@ -62,13 +64,22 @@ def _xas_factory(energy_grid, edge, key):
 @add_to_func_list
 def load_xas(filename):
     """
-    Load XAS plans from a TOML file.
+    Load XAS plans from a TOML file and inject them into the IPython user namespace.
 
     Parameters
     ----------
     filename : str
         Path to the TOML file containing XAS plan definitions
     """
+    try:
+        # Get IPython's user namespace
+        ip = get_ipython()
+        user_ns = ip.user_ns
+    except (NameError, AttributeError):
+        # Not running in IPython, just return the generated functions
+        user_ns = None
+
+    generated_plans = {}
     with open(filename, "rb") as f:
         regions = tomllib.load(f)
         for key, value in regions.items():
@@ -77,3 +88,13 @@ def load_xas(filename):
             edge = value.get("edge", "")
             xas_func = _xas_factory(region, edge, key)
             add_to_xas_list(xas_func, key, name=name, edge=edge, region=region)
+
+            # Store the function
+            generated_plans[key] = xas_func
+
+            # If we're in IPython, inject into user namespace
+            if user_ns is not None:
+                user_ns[key] = xas_func
+
+    # Return the generated plans dictionary in case it's needed
+    return generated_plans
