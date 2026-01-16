0cdef645c428d2aef6ad550ed2115f2d31431a0b
Charles Titus <ctitus@bnl.gov>
2026-01-16 16:26:15 -0500

    Trying to load a device which is not deferred should not cause an error

--- src/nbs_bl/beamline.py
+++ src/nbs_bl/beamline.py
@@ -230,7 +230,8 @@ class BeamlineModel:
             If loading the device fails
         """
         if device_name not in self._deferred_devices:
-            raise KeyError(f"Device {device_name} is not in deferred devices")
+            print(f"Device {device_name} is not in deferred devices")
+            return
 
         # If it's an alias, get and load the root device
         config = self._deferred_config.get(device_name, {})
